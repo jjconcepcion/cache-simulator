d139937c765db839181ec5943e8995e4efde9af5
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-04-22 03:00:11 -0700

    Parse lines from tracefile
    Define AccessDetail class to encapsulate the details of each instruction
    memory access: instruction address, access type, memory address, and
    size of access in bytes. Value of data is not needed for simulation and
    so is ignored.

--- sys1.cc
+++ sys1.cc
@@ -1,10 +1,51 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <cstdlib>
 #include <unistd.h>
+
+class AccessDetail {
+public:
+    uint32_t instrAddress;
+    uint32_t memAddress;
+    uint32_t numOfBytes;
+    bool memRead;
+    bool memWrite;
+};
 
 void usage(char *baseName) {
     std::cerr << "Usage: " << baseName << " tracefile " << "cachesize ";
     std::cerr << "[-v ic1 ic2]" << std::endl;
+}
+
+void parseLine(const std::string &line, AccessDetail &access) {
+    char accessType;
+
+    std::stringstream sstream(line);
+    sstream >> std::hex >> access.instrAddress;
+    sstream.ignore(1, ':');
+    sstream >> accessType;
+    sstream >> std::hex >> access.memAddress;
+    sstream >> std::dec >> access.numOfBytes;
+
+    access.memRead = (accessType == 'R');
+    access.memWrite = (accessType == 'W');
+}
+
+void simulate(const char *traceFilePath) {
+    std::ifstream traceFile;
+    AccessDetail access;
+    std::string line;
+
+    traceFile.open(traceFilePath, std::ifstream::in);
+    if (traceFile.fail()) {
+        std::cerr << "Error: failed to open: " << traceFilePath << std::endl;
+        exit(1);
+    }
+
+    while (std::getline(traceFile, line)) {
+        parseLine(line, access);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -38,6 +79,8 @@ int main(int argc, char *argv[]) {
         ic1 = atoi(argv[optind + 2]);
         ic2 = atoi(argv[optind + 3]);
     }
+
+    simulate(traceFilePath);
 
     return 0;
 }
